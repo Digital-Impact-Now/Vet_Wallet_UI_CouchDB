efcca8816feaac06102561615675df7751b53458
web_DigitalImpactNow <chris.dervan@digitalimpactnow.com>
2023-02-23 23:53:57 -0500

    bug handling

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import path from 'path';
+// import path from 'path';
 import TxtData from "./TxtData";
 
 //require('dotenv').config({path: path.resolve(__dirname, "../.env")})
@@ -9,156 +9,159 @@ import TxtData from "./TxtData";
 const baseURL = process.env.REACT_APP_FABRIC_BASEURL;
 
 function Home() {
-    const [Balance, setBalance] = useState();
-    const [ID, setID] = useState("");
-    const [txData, setTxData] = useState([])
+  const [Balance, setBalance] = useState();
+  const [ID, setID] = useState("");
+  const [txData, setTxData] = useState([]);
 
-    sessionStorage.setItem('userBalance', Balance);
+  sessionStorage.setItem("userBalance", Balance);
 
-    useEffect(() => {
-        
-        fetch("/express_backend").then(
-            res => res.text()
-        ).then(
-            data => {
-                console.log(data)
-                setTxData(data)
-                
-            } 
-        ).catch(
-            err => { console.log(err) }
-        )}, [])
+  useEffect(() => {
+    fetch("/express_backend")
+      .then((res) => res.text())
+      .then((data) => {
+        console.log(data);
+        setTxData(data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-    useEffect(() => {
-        fetch(baseURL + 'GetAccountBalance', {
-            method: "POST",
-            body: JSON.stringify({
-                fabricUserName: sessionStorage.getItem('fabricUserName')
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
-        })
-            // Converting to JSON
-            .then(response => response.json())
-            // Setting balance value to response value
-            .then(json => setBalance(json));
-    }, [Balance])
+  useEffect(() => {
+    fetch(baseURL + "GetAccountBalance", {
+      method: "POST",
+      body: JSON.stringify({
+        fabricUserName: sessionStorage.getItem("fabricUserName"),
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      // Converting to JSON
+      .then((response) => response.json())
+      // Setting balance value to response value
+      .then((json) => setBalance(json));
+  }, [Balance]);
 
-    useEffect(() => {
-        fetch(baseURL + 'GetID', {
-            method: "POST",
-            body: JSON.stringify({
-                fabricUserName: sessionStorage.getItem('fabricUserName')
-            }),
-            headers: {
-                "Content-type": "application/json; charset=UTF-8"
-            }
-        })
-            // Converting to JSON
-            .then(response => {
-                response.text().then(function (text) {
-                    setID(text)
-                });
-            })
-            .catch(e => {
-                console.log(e);
-            });
-    }, [])
+  useEffect(() => {
+    fetch(baseURL + "GetID", {
+      method: "POST",
+      body: JSON.stringify({
+        fabricUserName: sessionStorage.getItem("fabricUserName"),
+      }),
+      headers: {
+        "Content-type": "application/json; charset=UTF-8",
+      },
+    })
+      // Converting to JSON
+      .then((response) => {
+        response.text().then(function (text) {
+          setID(text);
+        });
+      })
+      .catch((e) => {
+        console.log(e);
+      });
+  }, []);
 
-    console.log(txData)
+  console.log(txData);
 
-    //Import JSON string
-    var stringJSON = JSON.stringify(txData);
+  //Import JSON string
+  var stringJSON = JSON.stringify(txData);
 
-    //Remove invalid characters & transactions text
-    stringJSON = stringJSON.replace(/\n|\r/g, "");
-    stringJSON = stringJSON.replaceAll('\\', '');
-    stringJSON = stringJSON.replace('"{"transactions":[', '');
-    stringJSON = stringJSON.slice(0, -3);
+  //Remove invalid characters & transactions text
+  stringJSON = stringJSON.replace(/\n|\r/g, "");
+  stringJSON = stringJSON.replaceAll("\\", "");
+  stringJSON = stringJSON.replace('"{"transactions":[', "");
+  stringJSON = stringJSON.slice(0, -3);
 
-    //Innitialize JSON to object variables
-    const objArray = [];
-    var jsonObj;
-    var obj
+  //Innitialize JSON to object variables
+  const objArray = [];
+  var jsonObj;
+  var obj;
 
-    while ((stringJSON.match(/id/g) || []).length > 2) {
-        //Parse JSON to object
-        jsonObj = stringJSON.substr(0, stringJSON.indexOf(',{"id":'));
-
-        //Push object to array
-        try {
-            obj = JSON.parse(jsonObj);
-            objArray.push(obj);
-        } catch (error) {
-            console.error(error);
-        }
-
-        //Remove pushed object from JSON txt
-        stringJSON = stringJSON.replace(',', '');
-        stringJSON = stringJSON.substring(jsonObj.length);
-        console.log(stringJSON);
-    }
+  while ((stringJSON.match(/id/g) || []).length > 2) {
+    //Parse JSON to object
+    jsonObj = stringJSON.substr(0, stringJSON.indexOf(',{"id":'));
 
     //Push object to array
     try {
-        obj = JSON.parse(stringJSON);
-        objArray.push(obj);
+      obj = JSON.parse(jsonObj);
+      objArray.push(obj);
     } catch (error) {
-        console.error(error);
+      console.error(error);
     }
-    
-    //Log object array
-    console.log(objArray);
 
-    return (
-        <div className="home-page">
-            <header className="home-header">
-                <div className="header-home-title">
-                    <p className="username">{sessionStorage.getItem('fabricUserName')}</p>
-                    <p className="address">{ID}</p>
-                </div>
-                <button className="account-link" onClick={() => window.location = './AccountScreen'}>
-                    Account
-                </button>
-            </header>
+    //Remove pushed object from JSON txt
+    stringJSON = stringJSON.replace(",", "");
+    stringJSON = stringJSON.substring(jsonObj.length);
+    console.log(stringJSON);
+  }
 
-            <div className="balance">
-                <h2 className="balance-title">Balance:</h2>
-                <form className="balance-box">
-                    <output><p className="balance-amt">{Balance}</p></output>
-                </form>
-                <button className="send-btn" onClick={() => window.location = './Send'}>
-                    Send →
-                </button>
-            </div>
+  //Push object to array
+  try {
+    obj = JSON.parse(stringJSON);
+    objArray.push(obj);
+  } catch (error) {
+    console.error(error);
+  }
 
-            <div className="tx-wrapper">
-                {objArray.map((tx, i) => 
-                    <div key={i}>
-                        {/* <p>Name: {tx.value.name}</p>
+  //Log object array
+  console.log(objArray);
+
+  return (
+    <div className="home-page">
+      <header className="home-header">
+        <div className="header-home-title">
+          <p className="username">{sessionStorage.getItem("fabricUserName")}</p>
+          <p className="address">{ID}</p>
+        </div>
+        <button
+          className="account-link"
+          onClick={() => (window.location = "./AccountScreen")}
+        >
+          Account
+        </button>
+      </header>
+
+      <div className="balance">
+        <h2 className="balance-title">Balance:</h2>
+        <form className="balance-box">
+          <output>
+            <p className="balance-amt">{Balance}</p>
+          </output>
+        </form>
+        <button
+          className="send-btn"
+          onClick={() => (window.location = "./Send")}
+        >
+          Send →
+        </button>
+      </div>
+
+      <div className="tx-wrapper">
+        {objArray.map((tx, i) => (
+          <div key={i}>
+            {/* <p>Name: {tx.value.name}</p>
                         <p>Wallet: {tx.value.wAddress}</p>
                         <p>Price: {tx.value.price}</p>
                         <p>Location: {tx.value.loc}</p>
                         <p>Timestamp: {tx.value.timestamp}</p>
                         <p>Tx ID: {tx.value.tx_id}</p> */}
 
-                        {
-                            
-                            <div className="tx-box">
-                                <h2 className="tx-type"> EBT purchase </h2>
-                                <p className="tx-date"> { tx.value.timestamp } </p>
-                                {/* <p> className="tx-timestamp">ti:me</p> */}
-                                <p className="tx-loc"> { tx.value.location } </p>
-                                <p className="tx-amount"> $ { tx.value.price } </p>
-                            </div>
-                             
-                        }
-                    </div>
-                )}
-            </div>
-
-        </div>
-    );
+            {
+              <div className="tx-box">
+                <h2 className="tx-type"> EBT purchase </h2>
+                <p className="tx-date"> {tx.value.timestamp} </p>
+                {/* <p> className="tx-timestamp">ti:me</p> */}
+                <p className="tx-loc"> {tx.value.location} </p>
+                <p className="tx-amount"> $ {tx.value.price} </p>
+              </div>
+            }
+          </div>
+        ))}
+      </div>
+    </div>
+  );
 }
 export default Home;
