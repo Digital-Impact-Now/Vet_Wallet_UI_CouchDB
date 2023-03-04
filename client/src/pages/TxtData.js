2a0e6bb3fa3921ee3d9ed67f1d5f97578ad4cb99
web_DigitalImpactNow <chris.dervan@digitalimpactnow.com>
2023-03-04 05:37:46 -0500

    commented everything out, will put to use later

--- client/src/pages/TxtData.js
+++ client/src/pages/TxtData.js
@@ -1,64 +1,42 @@
-import React, {useEffect, useState} from "react";
+// import React, {useEffect, useState} from "react";
 
-function TxtData() {
-    // const [backendData, setBackendData] = useState([{}])
+// function TxtData() {
 
-    // useEffect(() => {
-    //     fetch("/express_backend").then(
-    //         response => response.json()
-    //     ).then(
-    //         data => {
-    //             setBackendData(data)
-    //         }
-    //     )
-    // }, [])
+//     return (
+//         <div>
+//             <div className="tx-wrapper">
 
-    // {(typeof backendData.transactions === 'undefined') ? (
-    //     <p>Loading...</p>
-    // ) : (backendData.users.map((user,i) => (
-    //     <p key={i}>{user}</p>
-    // ))
-    // )}
-    
-    return (
-        <div>
-            <div className="tx-wrapper">
+//                 <div className="tx-box">
+//                 <h2 className="tx-type">purchase type</h2>
+//                 <p className="tx-date">month date,</p>
+//                 <p className="tx-timestamp">ti:me</p>
+//                 <p className="tx-loc">state</p>
+//                 <p className="tx-amount">$amount</p>
+//                 </div>
 
-                <div className="tx-box">
-                <h2 className="tx-type">purchase type</h2>
-                <p className="tx-date">month date,</p>
-                <p className="tx-timestamp">ti:me</p>
-                <p className="tx-loc">state</p>
-                <p className="tx-amount">$amount</p>
-                </div>
+//                 {/* {typeof backendData.data.forEach(data => {
+//                         <div>
+//                             <h2 className="tx-type">purchase type</h2>
+//                             <p className="tx-date">month date,</p>
+//                             <p className="tx-timestamp">ti:me</p>
+//                             <p className="tx-loc">state</p>
+//                             <p className="tx-amount">$amount</p>
+//                         </div>
+//                     })
+//                 } */}
 
-                {/* {typeof backendData.data.forEach(data => {
-                        <div>
-                            <h2 className="tx-type">purchase type</h2>
-                            <p className="tx-date">month date,</p>
-                            <p className="tx-timestamp">ti:me</p>
-                            <p className="tx-loc">state</p>
-                            <p className="tx-amount">$amount</p>
-                        </div>
-                    })
-                } */}
+//                 {/* {
+//                     (typeof backendData.transactions === 'undefined') ? (
+//                         <p>Loading...</p>
+//                     ) : (backendData.users.map((user, i) => (
+//                         <p key={i}>{user}</p>
+//                         ))
+//                     )
+//                 } */}
 
-                
+//             </div>
+//         </div>
+//     );
+// }
 
-                {/* {
-                    (typeof backendData.transactions === 'undefined') ? (
-                        <p>Loading...</p>
-                    ) : (backendData.users.map((user, i) => (
-                        <p key={i}>{user}</p>
-                        ))
-                    )
-                } */}
-                        
-
-                
-            </div>
-        </div>
-    );
-}
-
-export default TxtData;
+// export default TxtData;
