ef83a9f0d688a16dd366087c5f1c16e8c8626ec2
web_DigitalImpactNow <chris.dervan@digitalimpactnow.com>
2023-03-05 17:30:05 -0500

    this works for now

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,24 +1,13 @@
 import React, { useState, useEffect } from "react";
+import TxtData from "./TxtData";
 
 const baseURL = process.env.REACT_APP_FABRIC_BASEURL;
 
 function Home() {
   const [Balance, setBalance] = useState();
   const [ID, setID] = useState("");
-  const [txData, setTxData] = useState([]);
 
   sessionStorage.setItem("userBalance", Balance);
-
-  useEffect(() => {
-    fetch("/express_backend")
-      .then((res) => res.text())
-      .then((data) => {
-        setTxData(data);
-      })
-      .catch((err) => {
-        console.log(err);
-      });
-  }, []);
 
   useEffect(() => {
     fetch(baseURL + "GetAccountBalance", {
@@ -57,54 +46,6 @@ function Home() {
       });
   }, []);
 
-  //Import JSON string
-  var stringJSON = JSON.stringify(txData);
-
-  //Remove invalid characters & transactions text
-  stringJSON = stringJSON.replace(/\n|\r/g, "");
-  stringJSON = stringJSON.replaceAll("\\", "");
-  stringJSON = stringJSON.replace('"{"transactions":[', "");
-  stringJSON = stringJSON.slice(0, -3);
-
-  //Innitialize JSON to object variables
-  const objArray = [];
-  var jsonObj;
-  var obj;
-
-  while ((stringJSON.match(/id/g) || []).length > 2) {
-    //Parse JSON to object
-    jsonObj = stringJSON.substr(0, stringJSON.indexOf(',{"id":'));
-
-    //Push object to array
-    try {
-      obj = JSON.parse(jsonObj);
-      objArray.push(obj);
-    } catch (error) {
-      console.error(error);
-    }
-
-    //Remove pushed object from JSON txt
-    stringJSON = stringJSON.replace(",", "");
-    stringJSON = stringJSON.substring(jsonObj.length);
-    // console.log(stringJSON);
-  }
-
-  //Push object to array
-  try {
-    obj = JSON.parse(stringJSON);
-    objArray.push(obj);
-  } catch (error) {
-    console.error(error);
-  }
-
-  const userArray = objArray.filter(
-    (tx) => tx.value.name === sessionStorage.getItem("fabricUserName")
-  );
-
-  //Log object array
-  console.log("obj" + objArray);
-  console.log("usr:" + userArray);
-
   return (
     <div className="home-page">
       <header className="home-header">
@@ -134,21 +75,7 @@ function Home() {
           Send →
         </button>
       </div>
-
-      <div className="tx-wrapper">
-        {userArray.map((tx, i) => (
-          <div key={i}>
-            {
-              <div className="tx-box">
-                <h2 className="tx-type"> EBT purchase </h2>
-                <p className="tx-date"> {tx.value.timestamp} </p>
-                <p className="tx-loc"> {tx.value.location} </p>
-                <p className="tx-amount"> $ {tx.value.price} </p>
-              </div>
-            }
-          </div>
-        ))}
-      </div>
+      <TxtData />
     </div>
   );
 }
--- client/src/pages/TxtData.js
+++ client/src/pages/TxtData.js
@@ -1,42 +1,100 @@
-// import React, {useEffect, useState} from "react";
+import React, { useEffect, useState } from "react";
 
-// function TxtData() {
+function TxtData() {
+  const [txData, setTxData] = useState([]);
+  const [userArr, setUserArr] = useState([]);
 
-//     return (
-//         <div>
-//             <div className="tx-wrapper">
+  useEffect(() => {
+    fetch("/express_backend")
+      .then((res) => res.text())
+      .then((data) => {
+        setTxData(data);
 
-//                 <div className="tx-box">
-//                 <h2 className="tx-type">purchase type</h2>
-//                 <p className="tx-date">month date,</p>
-//                 <p className="tx-timestamp">ti:me</p>
-//                 <p className="tx-loc">state</p>
-//                 <p className="tx-amount">$amount</p>
-//                 </div>
+        const stringJSON = JSON.stringify(data);
 
-//                 {/* {typeof backendData.data.forEach(data => {
-//                         <div>
-//                             <h2 className="tx-type">purchase type</h2>
-//                             <p className="tx-date">month date,</p>
-//                             <p className="tx-timestamp">ti:me</p>
-//                             <p className="tx-loc">state</p>
-//                             <p className="tx-amount">$amount</p>
-//                         </div>
-//                     })
-//                 } */}
+        const cleanedJSON = cleanRawData(stringJSON);
+        const objArr = buildArray(cleanedJSON);
 
-//                 {/* {
-//                     (typeof backendData.transactions === 'undefined') ? (
-//                         <p>Loading...</p>
-//                     ) : (backendData.users.map((user, i) => (
-//                         <p key={i}>{user}</p>
-//                         ))
-//                     )
-//                 } */}
+        const userArray = objArr.filter(
+          (tx) => tx.value.name === sessionStorage.getItem("fabricUserName")
+        );
+        setUserArr(userArray);
 
-//             </div>
-//         </div>
-//     );
-// }
+        //Log object array
+        console.log("obj" + objArr);
+        console.log("usr:" + userArray);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, []);
 
-// export default TxtData;
+  const cleanRawData = (stringJSON) => {
+    //Import JSON string
+
+    console.log("raw: " + stringJSON);
+
+    //Remove invalid characters & transactions text
+    let cleanedJSON = stringJSON.replace(/\n|\r/g, "");
+    cleanedJSON = cleanedJSON.replaceAll("\\", "");
+    cleanedJSON = cleanedJSON.replace('"{"transactions":[', "");
+    cleanedJSON = cleanedJSON.slice(0, -3);
+    return cleanedJSON;
+  };
+
+  const buildArray = (cleanedJSON) => {
+    //Innitialize JSON to object variables
+    const objArray = [];
+    let jsonObj;
+    let obj;
+
+    while ((cleanedJSON.match(/id/g) || []).length > 2) {
+      //Parse JSON to object
+      jsonObj = cleanedJSON.substr(0, cleanedJSON.indexOf(',{"id":'));
+
+      //Push object to array
+      try {
+        obj = JSON.parse(jsonObj);
+        objArray.push(obj);
+      } catch (error) {
+        console.error(error);
+      }
+
+      //Remove pushed object from JSON txt
+      cleanedJSON = cleanedJSON.replace(",", "");
+      cleanedJSON = cleanedJSON.substring(jsonObj.length);
+      console.log("cleaned: " + cleanedJSON);
+    }
+
+    //Push object to array
+    try {
+      obj = JSON.parse(cleanedJSON);
+      objArray.push(obj);
+    } catch (error) {
+      console.error(error);
+    }
+
+    return objArray;
+  };
+
+  return (
+    <div>
+      <div className="tx-wrapper">
+        {userArr.map((tx, i) => (
+          <div key={i}>
+            {
+              <div className="tx-box">
+                <h2 className="tx-type"> EBT purchase </h2>
+                <p className="tx-date"> {tx.value.timestamp} </p>
+                <p className="tx-loc"> {tx.value.location} </p>
+                <p className="tx-amount"> $ {tx.value.price} </p>
+              </div>
+            }
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export default TxtData;
